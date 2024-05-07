35a36c46155e6766b3362e0fabb54a9694320e9c
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2024-05-07 15:02:53 +0700

    installing clockwork for performace debug

---
+++ resources/views/dashboard/partials/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="footer-left">
+        Copyright &copy; 2024 <div class="bullet"></div> Rentfox V2.0</a>
+    </div>
+    <div class="footer-right">
+
+    </div>
+</footer>
--- resources/views/dashboard/partials/sidebar.blade.php
+++ resources/views/dashboard/partials/sidebar.blade.php
@@ -1,10 +1,10 @@
 <div class="main-sidebar sidebar-style-2">
     <aside id="sidebar-wrapper">
         <div class="sidebar-brand">
-            <a href="/">Stisla</a>
+            <a href="/">RentFox</a>
         </div>
         <div class="sidebar-brand sidebar-brand-sm">
-            <a href="/">St</a>
+            <a href="/">RFX</a>
         </div>
         <ul class="sidebar-menu">
             <li class="menu-header">Dashboard</li>
