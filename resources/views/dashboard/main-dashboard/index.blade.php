c8708a8bcfa7187c4b935211310c4db92420e10c
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2024-05-07 14:45:27 +0700

    Add dashboard template stisla

---
+++ resources/views/dashboard/main-dashboard/index.blade.php
@@ -0,0 +1,10 @@
+@extends('dashboard.layouts.main')
+@section('content')
+<div class="section-header">
+    <h1>Dashboard Utama</h1>
+  </div>
+
+  <div class="section-body">
+    
+  </div>
+@endsection
